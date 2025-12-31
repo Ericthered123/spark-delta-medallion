a8016ee91a241446e89f5a7a8b75bc58a506e4cf
Ericthered123 <eric6doyle@gmail.com>
2025-12-31 13:55:06 -0300

    fix spark_session

--- src/common/spark_session.py
+++ src/common/spark_session.py
@@ -1,18 +1,30 @@
 
+
+
+
+
+"""
+    Create and return a SparkSession configured with Delta Lake support.
+    Centralizing SparkSession creation ensures consistent configuration
+    across all pipelinae layers (Bronze, Silver, Gold).
+"""
+   
 from pyspark.sql import SparkSession
 from delta import configure_spark_with_delta_pip
 
-
 def get_spark(app_name: str) -> SparkSession:
-    """
-    Create and return a SparkSession configured with Delta Lake support.
-    Centralizing SparkSession creation ensures consistent configuration
-    across all pipeline layers (Bronze, Silver, Gold).
-    """
     builder = (
         SparkSession.builder
         .appName(app_name)
         .master("local[*]")
+        .config(
+            "spark.sql.extensions",
+            "io.delta.sql.DeltaSparkSessionExtension"
+        )
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog"
+        )
     )
 
     spark = configure_spark_with_delta_pip(builder).getOrCreate()
